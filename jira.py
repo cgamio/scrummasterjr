596b7bb7bf8b99efbea7e095a4cbbf9823645a20
Chris Gamio <chris.gamio@hearst.com>
2020-03-04 13:53:24 -0500

    Boilerplate for `sprint metrics` command

--- jira.py
+++ jira.py
@@ -1,6 +1,7 @@
 from requests.auth import HTTPBasicAuth
 import requests
 import logging
+import re
 
 class Jira:
     __auth = None
@@ -29,14 +30,33 @@ class Jira:
             return "My connection to Jira is up and running!"
         else:
             logging.error(f"Error with Jira connection: {response}")
-            return "Looks like there's an issue with my connection..."
+            return "Looks like there's an issue with my connection. I've logged an error"
+
+    def __getSprintMetrics(self, id):
+        raise Exception("This function hasn't been implemented yet!")
+
+        return response
+
+    def getSprintMetricsCommand(self, message):
+        sprintid = re.search('sprint metrics ([0-9]+)', message.get("text")).group(1)
+
+        try:
+            response = self.__getSprintMetrics(sprintid)
+        except BaseException as e:
+            logging.error(f"There was an error generating sprint metrics for sprint {sprintid}\n{str(e)}")
+            return "Sorry, I had trouble getting metrics for that sprint. I've logged an error"
+
+        return response
+
 
     def getCommandsRegex(self):
         return {
-            'test jira': self.testConnectionCommand
+            'test jira': self.testConnectionCommand,
+            'sprint metrics [0-9]+': self.getSprintMetricsCommand
         }
 
     def getCommandDescriptions(self):
         return {
-            'test jira': 'tests my connection to jira'
+            'test jira': 'tests my connection to jira',
+            'sprint metrics [sprint-id]': 'get metrics for a given sprint'
         }
