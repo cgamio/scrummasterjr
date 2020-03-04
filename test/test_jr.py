6b4f84678f3154aabae3f3456c53574d787c1224
Chris Gamio <chris.gamio@hearst.com>
2020-03-04 16:26:42 -0500

    tests for help and health functions

--- test/test_jr.py
+++ test/test_jr.py
@@ -10,3 +10,20 @@ def test_healthcheck(client):
     rv = client.get('/health')
 
     assert 200 is rv.status_code
+
+def test_help(client):
+    default_response = "These are the things I know how to respond to:\nhello - random greeting"
+
+    jr.commandsets = []
+    response = jr.get_help("help")
+    assert response == default_response
+
+    class TestSet:
+        def getCommandDescriptions(self):
+            return {"some command": "does a test thing"}
+
+    set = TestSet()
+    jr.commandsets = [set]
+    expected_response = f"{default_response}\nsome command - does a test thing"
+    response = jr.get_help("help")
+    assert response == expected_response
