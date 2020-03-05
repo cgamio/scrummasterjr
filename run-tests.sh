a6f18e1ff065f75569d80b984e36762e83d4e853
Chris Gamio <chris.gamio@hearst.com>
2020-03-05 17:02:55 -0500

    set test env vars in run-tests.sh script and use that for travis tests

---
+++ run-tests.sh
@@ -0,0 +1,7 @@
+export SLACK_SIGNING_SECRET="test-signing-secret"
+export SLACK_BOT_TOKEN="test-slack-bot-token"
+export JIRA_HOST="test-jira-host"
+export JIRA_USER="test-jira-user"
+export JIRA_TOKEN="test-jira-token"
+
+python -m pytest
