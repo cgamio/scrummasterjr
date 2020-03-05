89e792fab430e0d829ed143e5a4ba8cb68a28e03
Chris Gamio <chris.gamio@hearst.com>
2020-03-05 15:50:51 -0500

    Update tests to reflect implementation changes

--- test/test_jira.py
+++ test/test_jira.py
@@ -7,11 +7,10 @@ jira = jira.Jira(" ", " ", " ")
 @patch('jira.requests')
 @pytest.mark.parametrize('http_code , expected_response', [
     (200, "My connection to Jira is up and running!"),
-    (500, "Looks like there's an issue with my connection...")
+    (500, "Looks like there's an issue with my connection. I've logged an error")
 ])
 def test_testConnectionCommand(mock_requests, http_code, expected_response):
-    mock_requests.request.return_value = MagicMock(status_code=http_code)
-
+    mock_requests.request.return_value = MagicMock(status_code=http_code, text='{"text": "some mock test"}')
 
     response = jira.testConnectionCommand("")
 
@@ -19,14 +18,16 @@ def test_testConnectionCommand(mock_requests, http_code, expected_response):
 
 def test_getCommandsRegex():
     expected_response = {
-        'test jira': jira.testConnectionCommand
+        'test jira': jira.testConnectionCommand,
+        'sprint metrics [0-9]+': jira.getSprintMetricsCommand
     }
 
     assert jira.getCommandsRegex() == expected_response
 
 def test_getCommandDescriptions():
     expected_response = {
-            'test jira': 'tests my connection to jira'
+        'test jira': 'tests my connection to jira',
+        'sprint metrics [sprint-id]': 'get metrics for a given sprint'
     }
 
     assert jira.getCommandDescriptions() == expected_response
