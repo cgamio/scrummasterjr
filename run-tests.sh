1e270858728ff620666abea73f41a375a2147916
Niyi Odumosu <Niyi.Odumosu@hearst.com>
2020-03-06 08:28:11 -0500

    Modified test script and to include env vars in docker run command

--- run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export SLACK_SIGNING_SECRET="test-signing-secret"
-export SLACK_BOT_TOKEN="test-slack-bot-token"
-export JIRA_HOST="test-jira-host"
-export JIRA_USER="test-jira-user"
-export JIRA_TOKEN="test-jira-token"
-
-python -m pytest
