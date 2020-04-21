0e8ef3c147a29998d98095438466786d8634db01
Chris Gamio <chris.gamio@hearst.com>
2020-04-21 11:57:46 -0400

    Generate Google Form URL

--- test/test_jira.py
+++ test/test_jira.py
@@ -981,7 +981,7 @@ def test_generateAllSprintReportData(mock_requests, sprint_id, sprint_get_respon
     else:
         assert jira.generateAllSprintReportData(sprint_id) == expected_response
 
-valid_google_form_url='https://docs.google.com/forms/d/e/1FAIpQLSdF__V1ZMfl6H5q3xIQhSkeZMeCNkOHUdTBFdYA1HBavH31hA/viewform?'
+valid_google_form_url='https://docs.google.com/forms/d/e/1FAIpQLSdF__V1ZMfl6H5q3xIQhSkeZMeCNkOHUdTBFdYA1HBavH31hA/viewform?entry.1082637073=TBT&entry.1975251686=1&entry.448087930=1&entry.2095001800=2&entry.1399119358=2&entry.128659456=0&entry.954885633=2&entry.1137054034=0&entry.1980453543=1&entry.1252702382=0&entry.485777497=0&entry.370334542=0&entry.1427603868=6&entry.1486076673=6&entry.254612996=3&entry.611444996=0&entry.2092919144=0&entry.493624591=6&entry.976792423=0&entry.1333444050=0&'
 
 @pytest.mark.parametrize('sprint_report_data, expected_response', [
     (valid_report, valid_google_form_url),
