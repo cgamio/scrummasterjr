04e9a681eef15f17d4eaedb0c3c590ab0d6261ad
Chris Gamio <chris.gamio@hearst.com>
2020-04-23 15:40:30 -0400

    Hopefully hooking travis up to run code coverage stuff

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-python -m pytest
+python -m pytest -vv --cov-report=xml --cov-report=html --cov=scrum_master_jr --cov=jira --cov-fail-under=90
+
+if [ "$CODECOV" == "true" ]; then
+    bash <(curl -s https://codecov.io/bash);
+fi
