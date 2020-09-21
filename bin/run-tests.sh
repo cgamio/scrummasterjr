a64d1f84327709d66686b7b1510eda335e3de699
Chris Gamio <chris.gamio@hearst.com>
2020-09-21 16:32:06 -0400

    Exit when any command in this script fails

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
+set -e
 
 python -m pytest -vv --cov-report=xml --cov-report=html --cov=scrum_master_jr --cov=jira --cov-fail-under=90
-
-RETURN=$?
-
-if [ "$RETURN" != "0" ]; then
-  exit $RETURN
-fi
 
 if [ "$CODECOV" == "true" ]; then
     bash <(curl -s https://codecov.io/bash);
