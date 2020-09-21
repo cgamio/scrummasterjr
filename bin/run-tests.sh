c0f2732315e7d2be741ce45fde3db0ed31e33d9b
Chris Gamio <chris.gamio@hearst.com>
2020-09-21 16:23:41 -0400

    Bail with an error if the tests fail

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -2,6 +2,12 @@
 
 python -m pytest -vv --cov-report=xml --cov-report=html --cov=scrum_master_jr --cov=jira --cov-fail-under=90
 
+RETURN=$?
+
+if [ "$RETURN" != "0" ]; then
+  exit $RETURN
+fi
+
 if [ "$CODECOV" == "true" ]; then
     bash <(curl -s https://codecov.io/bash);
 fi
