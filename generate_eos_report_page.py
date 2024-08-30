18b60379210f03e249d8435be62ea2c529475536
Chris Gamio <cgamio@cap-rx.com>
2024-08-30 16:47:11 -0400

    Determine Sprint from Confluence and bail when early

--- generate_eos_report_page.py
+++ generate_eos_report_page.py
@@ -14,24 +14,39 @@ if __name__ == '__main__':
         password=os.environ["JIRA_TOKEN"]
     )
 
-    current_sprint_string = os.environ["CURRENT_SPRINT"]
-    current_sprint_split = current_sprint_string.split('.', 1)
-    next_sprint = f"{current_sprint_split[0]}.{int(current_sprint_split[1])+1}"
-
-    # Assume that this is going to run on the Monday before the sprint ending on Wednesday
-    now = datetime.datetime.now()-datetime.timedelta(days=12)
-    start_date = now.strftime('%m/%d')
-    end_date = (now+datetime.timedelta(days=2)).strftime('%m\%d')
-
-    presenting_string = "A-K" if int(current_sprint_split[1])%2 else "J-Z"
-
     template_page_id = os.environ["TEMPLATE_PAGE_ID"]
     parent_page_id = os.environ["PARENT_PAGE_ID"]
 
-    source = confluence.get_page_by_id(template_page_id, expand='body.storage')
-    newbody = source['body']['storage']['value']
+    # Find the last EOS
+    children = confluence.get_child_pages(parent_page_id)
+    *_, last_child = children
 
-    newbody = re.sub(r'\[sprint {sprint number}\]', f'[sprint {current_sprint_string}]', newbody)
-    newbody = re.sub(r'\[next-sprint {sprint number}\]', f'[next-sprint {next_sprint}]', newbody)
+    match = re.search(r'(\d+) Report - (\d+\/\d+) to (\d+\/\d+)', last_child['title'])
+    if match: 
+        now = datetime.datetime.now()
+        previous_end = datetime.datetime.strptime(f"{match.group(3)}/{now.strftime('%y')}", "%m/%d/%y")
 
-    dest = confluence.update_or_create(parent_page_id, f"[PENDING METRICS] End of Sprint {current_sprint_split[1]} Report - {start_date} to {end_date} - {presenting_string} Presenting", newbody)
+        # Assume that this is going to run on the Monday before the sprint ending on Wednesday
+        if previous_end > now-datetime.timedelta(days=7):
+            print(f"Previous EOS ended on {match.group(3)} and we need to wait until next week to cut a new one")
+            exit(1)
+
+        current_sprint_number = int(match(1))+1
+        current_sprint_string = f"{now.strftime('%y')}.{current_sprint_number}"
+        next_sprint = f"{now.strftime('%y')}.{current_sprint_number+1}"
+        
+        start_date = (now-datetime.timedelta(days=12)).strftime('%m/%d')
+        end_date = (now+datetime.timedelta(days=2)).strftime('%m\%d')
+        
+        presenting_string = "A-K" if current_sprint_number%2 else "J-Z"
+
+        source = confluence.get_page_by_id(template_page_id, expand='body.storage')
+        newbody = source['body']['storage']['value']
+
+        newbody = re.sub(r'\[sprint {sprint number}\]', f'[sprint {current_sprint_string}]', newbody)
+        newbody = re.sub(r'\[next-sprint {sprint number}\]', f'[next-sprint {next_sprint}]', newbody)
+
+        dest = confluence.update_or_create(parent_page_id, f"[PENDING METRICS] End of Sprint {current_sprint_number} Report - {start_date} to {end_date} - {presenting_string} Presenting", newbody)
+    else: 
+        print("Last child in parent does not match EOS title formatting")
+        exit(1)
